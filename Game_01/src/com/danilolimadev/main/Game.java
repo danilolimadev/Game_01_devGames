9471c8d5d956621c02a5a08986fbdd928e0c2a9c
danilolimadev <danilolimadev@gmail.com>
2020-02-09 11:33:05 -0300

    First Commit

---
+++ Game_01/src/com/danilolimadev/main/Game.java
@@ -0,0 +1,190 @@
+package com.danilolimadev.main;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+import com.danilolimadev.entities.Entity;
+import com.danilolimadev.entities.Player;
+import com.danilolimadev.graficos.Spritesheet;
+
+public class Game extends Canvas implements Runnable, KeyListener{
+
+	private static final long serialVersionUID = 1L;
+	public static JFrame frame;
+	private Thread thread;
+	private boolean isRunning = true;
+	private final int WIDTH = 240;
+	private final int HEIGHT = 160;
+	private final int SCALE = 3;
+	
+	private BufferedImage image;
+	
+	public List<Entity> entities;
+	public static Spritesheet spritesheet;
+	
+	private Player player;
+	
+	public Game() {
+		addKeyListener(this);
+		setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		initFrame();
+		//Inicializando objetos
+		image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_ARGB);
+		entities = new ArrayList<Entity>();
+		spritesheet = new Spritesheet("/spritesheet.png");
+		//spritesheet = new Spritesheet("/L.png");
+		
+		player = new Player(0,0,16,16,spritesheet.getSprite(32, 0, 16, 16));
+		//player = new Player(0,0,16,16,spritesheet.getSprite(0, 0, 32, 46));
+		entities.add(player);
+	}
+	
+	public void initFrame() {
+		frame = new JFrame("Game #1");
+		frame.add(this);
+		frame.setResizable(false);	//Quando em falso não permite ao usuário alterar o tamanho da tela
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String args[]) {
+		Game game = new Game();
+		game.start();
+	}
+	
+	public void tick() {
+		for(int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.tick();
+		}
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();	//Para importar no atalho é control+shift+o
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = image.getGraphics();	
+		g.setColor(new Color(0,255,0));	//Fazer isso toda vez, limpa a tela e volta para a posição inicial para continuar desenhando
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		
+		/*Renderização do jogo*/
+		//Graphics2D g2 = (Graphics2D) g;
+		for(int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.render(g);
+		}
+		/**/
+		g.dispose();
+		g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+		bs.show();
+	}
+	
+	public void run() {
+		long lastTime = System.nanoTime();	//Obtem o tempo atual do computador em nano segundos
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+		while(isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if(delta>=1) {
+				tick();	//Atualizar o jogo
+				render();	//Renderizar
+				frames++;
+				delta--;
+			}
+			
+			if(System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				timer += 1000;
+			}
+		}
+		
+		stop();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT ||
+				e.getKeyCode() == KeyEvent.VK_D) {
+			player.right = true;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT ||
+				e.getKeyCode() == KeyEvent.VK_A) {
+			player.left = true;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_UP ||
+				e.getKeyCode() == KeyEvent.VK_W) {
+			player.up = true;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_DOWN ||
+				e.getKeyCode() == KeyEvent.VK_S) {
+			player.down = true;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT ||
+				e.getKeyCode() == KeyEvent.VK_D) {
+			player.right = false;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT ||
+				e.getKeyCode() == KeyEvent.VK_A) {
+			player.left = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_UP ||
+				e.getKeyCode() == KeyEvent.VK_W) {
+			player.up = false;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_DOWN ||
+				e.getKeyCode() == KeyEvent.VK_S) {
+			player.down = false;
+		}
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
